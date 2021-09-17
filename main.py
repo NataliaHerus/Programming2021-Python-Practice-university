42e17f71aab3540d92c9779b7ebc3d740b1aeafa
Natalia Herus <natalia.gerus892@gmail.com>
2021-09-17 19:19:11 +0300

    Find the amount of lucky numbers which consists of numbers enetered by the user (var 4)

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-def lucky_numbers(biggest_number):
-    lucky_numbers = []
-    array_to_help_find = []
+def lucky_numbers(biggest_number, your_condition):
+    found_lucky_numbers = []
     for numbers in range(biggest_number):
         string_to_find_figures = str(numbers)
+        is_lucky = True
         for position in range(len(string_to_find_figures)):
-            if '4' in string_to_find_figures[position] or '7' in string_to_find_figures[position]:
-                array_to_help_find.append(string_to_find_figures[position])
-            else:
-                array_to_help_find.clear()
+            is_lucky_digit = False
+            for i in range(len(your_condition)):
+                if string_to_find_figures[position] == your_condition[i]:
+                    is_lucky_digit = True
+                    continue
+            if not is_lucky_digit:
+                is_lucky = False
                 break
-
-        if len(array_to_help_find) != 0:
-            result = [int(item) for item in array_to_help_find]
-            lucky_numbers.append(result)
-    return len(lucky_numbers)
+        if is_lucky:
+            found_lucky_numbers.append(string_to_find_figures)
+    return len(found_lucky_numbers)
 
 
-def input_data():
+def number():
     while True:
         try:
             the_biggest_number = int(input('Input the biggest natural number: '))
             if the_biggest_number <= 0:
-                print('\nLucky numbers are bigger than 0')
+                print('\nLucky numbers are bigger bigger than 0')
                 continue
             break
         except ValueError:
@@ -29,9 +30,28 @@ def input_data():
     return the_biggest_number
 
 
-our_number = input_data()
-found_lucky_numbers = lucky_numbers(our_number)
+def all_for_our_array():
+    while True:
+        try:
+            some_numbers = []
+            n = int(input("Input size of array:"))
+            if n <= 0:
+                print('\nSize should be bigger than 0')
+                continue
+            for i in range(n):
+                array_element = int(input('Input an array element: '))
+                some_numbers.append(str(array_element))
+                continue
+            break
+        except ValueError:
+            print('\nPlease enter an number')
+    return some_numbers
+
+
+our_number = number()
+your_condition = all_for_our_array()
+result = lucky_numbers(our_number, your_condition)
 print('The amount of lucky numbers less than', our_number, ': ')
-if lucky_numbers(our_number) == 0:
+if lucky_numbers(our_number, your_condition) == 0:
     print('\nThere are no lucky numbers no larger than the number you entered')
-print(found_lucky_numbers)
+print(result)
